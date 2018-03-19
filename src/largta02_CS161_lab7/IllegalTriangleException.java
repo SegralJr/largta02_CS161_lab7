f24324c47709936417615a043a663e4faf0d5e2a
Taylor Large <segral.jr@gmail.com>
2018-03-19 13:45:28 -0400

    Initial commit

---
+++ src/largta02_CS161_lab7/IllegalTriangleException.java
@@ -0,0 +1,17 @@
+package largta02_CS161_lab7;
+
+public class IllegalTriangleException {
+	
+	/*
+	 * Taylor Large
+	 * CS161
+	 * Spring 2018
+	 * Lab 7
+	 */
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
